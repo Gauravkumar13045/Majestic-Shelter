0ba1c97b519d2573e500f5ca6cf106f876de6826
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-04-16 21:43:02 +0530

    Flip card added

---
+++ index.js
@@ -0,0 +1,12 @@
+
+  const video = document.getElementById('hoverVideo');
+
+  video.addEventListener('mouseover', () => {
+    video.play(); // Play video on hover
+  });
+
+  video.addEventListener('mouseout', () => {
+    video.pause(); // Pause video when hover stops
+  });
+
+
