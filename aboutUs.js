7d9d0c3445dd51db91c0d05efdadcf259cf47919
Gauravkumar13045 <gauravkumar13045@gmail.com>
2025-05-02 08:30:05 +0530

    About Us halfway completed

---
+++ aboutUs.js
@@ -0,0 +1,10 @@
+document.addEventListener("scroll", function () {
+    document.querySelectorAll(".scroll-animation").forEach((el) => {
+        let position = el.getBoundingClientRect().top;
+        let windowHeight = window.innerHeight;
+
+        if (position < windowHeight - 100) {
+            el.classList.add("show");
+        }
+    });
+});
--- index.js
+++ index.js
@@ -11,32 +11,7 @@ document.querySelector("video").addEventListener("mouseleave", () => {
   video.pause();
 });
 
-// countdown timer
 
-// Set the target end date and time for the countdown
-const targetDate = new Date("2025-05-01T23:59:59").getTime();
-
-// Update the countdown every second
-const countdownInterval = setInterval(() => {
-  const now = new Date().getTime();
-  const timeRemaining = targetDate - now;
-
-  if (timeRemaining > 0) {
-    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-    document.getElementById("days").innerText = days;
-    document.getElementById("hours").innerText = hours;
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
-  } else {
-    clearInterval(countdownInterval);
-    document.querySelector("h1").innerText = "Offer Has Ended!";
-    document.querySelector(".d-flex").style.display = "none";
-  }
-}, 1000);
 
 // Function to animate the count
 // Function to animate the count
@@ -108,6 +83,16 @@ if (toastTrigger) {
 }
 
 
+document.addEventListener("scroll", function () {
+  document.querySelectorAll(".scroll-animation").forEach((el) => {
+      let position = el.getBoundingClientRect().top;
+      let windowHeight = window.innerHeight;
+
+      if (position < windowHeight - 100) {
+          el.classList.add("show");
+      }
+  });
+});
 
 
 
